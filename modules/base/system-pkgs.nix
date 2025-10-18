f595c7e7912b1efc886d452da72ce3df69785e9d
Mereart1174 <mereart1174@163.com>
2025-10-18 18:14:56 +0800

    xxx

--- modules/base/system-pkgs.nix
+++ modules/base/system-pkgs.nix
@@ -34,7 +34,6 @@
     ldns # replacement of `dig`, it provide the command `drill`
     wget
     curl
-    # aria2 # A lightweight multi-protocol & multi-source command-line download utility
     socat # replacement of openbsd-netcat
     nmap # A utility for network discovery and security auditing
     ipcalc # it is a calculator for the IPv4/v6 addresses
--- modules/nixos/desktop/misc.nix
+++ modules/nixos/desktop/misc.nix
@@ -29,11 +29,13 @@
   services = {
     gvfs.enable = true; # Mount, trash, and other functionalities
     tumbler.enable = true; # Thumbnail support for images
+    transmission = {
+      enable = ture;
+    };
   };
 
   programs = {
     # dconf is a low-level configuration system.
     dconf.enable = true;
-
   };
 }
