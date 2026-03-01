ee1d641cbb3f8950336ae0073d9eb4f9a7395099
Mereart1174 <mereart1174@163.com>
2026-03-02 03:37:37 +0800

    noctalia battery

--- system/desktop/misc.nix
+++ system/desktop/misc.nix
@@ -27,6 +27,7 @@
 
 
   services = {
+    upower.enable = true;
     gvfs.enable = true; # Mount, trash, and other functionalities
     tumbler.enable = true; # Thumbnail support for images
     # resolved.enable = true;
