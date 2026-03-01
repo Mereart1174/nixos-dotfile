ee8aa6c40cb1adc960bb3d55b353e02d18479a49
Mereart1174 <mereart1174@163.com>
2026-03-01 10:13:04 +0800

    fcitx5

---
+++ home/desktop/base/fcitx5.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, lib, ... }:
+{
+  home.file.".local/share/fcitx5/rime/default.custom.yaml".text = ''
+    patch:
+      __include: rime_ice_suggestion:/
+      __patch:
+        # 横排显示
+        "style/horizontal": true
+        # 候选词数量
+        "menu/page_size": 7
+      schema_list:
+        - schema: rime_ice
+  '';
+}
--- system/desktop/fcitx5.nix
+++ system/desktop/fcitx5.nix
@@ -1,30 +1,14 @@
 { config, pkgs, ... }:
 {
-  # xdg.configFile = {
-  #   "fcitx5/rime" = {
-  #     source = pkgs.fetchFromGitHub {
-  #       owner = "iDvel";
-  #       repo = "rime-ice";
-  #
-  #       rev = "main";
-  #
-  #       sha256 = "u4KYZaaXDayPHa+TuMKoYoVEmOJm1egonAje+NF65aM=";
-  #     };
-  #     recursive = true;
-  #   };
-  # };
-
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
     fcitx5.waylandFrontend = true;
     fcitx5.addons = with pkgs; [
-      qt6Packages.fcitx5-configtool # GUI for fcitx5
-      fcitx5-gtk # gtk im module
+      qt6Packages.fcitx5-configtool
+      fcitx5-gtk
       fcitx5-fluent
 
-      # Chinese
-      # fcitx5-rime # for flypy chinese input method
       (fcitx5-rime.override {
         rimeDataPkgs = [
           pkgs.rime-ice
