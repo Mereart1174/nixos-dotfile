4b4b193514532fb33467c7f545a3dea50c472dd5
Mereart1174 <mereart1174@163.com>
2026-04-04 21:20:23 +0800

    nvidia

--- hosts/wsl/nvidia.nix
+++ hosts/wsl/nvidia.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 {
   # ===============================================================================================
   # for Nvidia GPU
@@ -30,4 +30,22 @@
     # needed by nvidia-docker
     enable32Bit = true;
   };
+
+  # 解决 "Could not start dynamically linked executable" 的核心配置
+  # nix-ld 会创建一个兼容层，让那些硬编码了动态链接路径的程序能找到库
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    stdenv.cc.cc
+    zlib
+    fuse3
+    icu
+    nss
+    openssl
+    curl
+    expat
+    # 如果你要运行预编译的 CUDA 程序，可能还需要这些：
+    linuxPackages.nvidia_x11
+    # libglow
+    glib
+  ];
 }
